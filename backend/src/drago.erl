f525b7771f52a03ae3f56e3ece4129bfaaa659e5
Dima <dmitriy.kirakosyan@gmail.com>
2011-07-26 09:14:10 +0400

    fix small bugs

--- backend/src/drago.erl
+++ backend/src/drago.erl
@@ -2,26 +2,28 @@
 
 -export([start_session/2]).
 
+-include("game.hrl").
+
 -define(interval, 5000).
 
 start_session(UserId, Sock) ->
 		State = drago_state:get_state(UserId),
-		GameSession = sessions_manager:register_session(UseId, Sock, self(), State),
+		GameSession = sessions_manager:register_session(UserId, Sock, self(), State),
 		loop(GameSession).
 
 loop(GameSession) ->
 		erlang:garbage_collect(self()),
-		case game_protocol:parse_event(Sock) of
-				{ok, {ActionId, Requets}} ->
+		case game_protocol:parse_event(GameSession#session.sock) of
+				{ok, {ActionId, Request}} ->
 						handle_event(GameSession, ActionId, Request),
 						loop(GameSession);
 				{error, timeout} ->
-						loop(GameState);
-				{error, Reason} ->
+						loop(GameSession);
+				{error, _Reason} ->
 						io:format("error in receive~n")
 		end.
 
-handle_event(GameSession, ActionId, Request) ->
+handle_event(GameSession, ActionId, _Request) ->
 		Encoded = mochijson2:encode({ok, response}),
 		send_event(GameSession#session.sock, {ActionId, iolist_to_binary(Encoded)}).
 
