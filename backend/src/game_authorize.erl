d95ed4b68e0b946bf82b1c5bf038d22b3639d525
Dima K <dmitriy.kirakosyan@gmail.com>
2011-08-08 00:18:18 +0400

    remove tabs

--- backend/src/game_authorize.erl
+++ backend/src/game_authorize.erl
@@ -3,18 +3,18 @@
 
 authorize(Sock, Module) ->
     case catch start_game(Sock, Module) of
-				{'EXIT', _Err} ->
-						auth_error;
-				_ ->
-						ok
+        {'EXIT', _Err} ->
+            auth_error;
+        _ ->
+            ok
     end,
     gen_tcp:close(Sock).
 
 start_game(Sock, Module) ->
     case game_protocol:parce_event(Sock) of
-				{ok, {_ActionId, FirstRequest}} ->
-						UserId = proplists:get_value(<<"user_id">>, FirstRequest),
-						Module:start_session(UserId, Sock);
-				_ ->
-						{error, cant_parse}
-		end.
+        {ok, {_ActionId, FirstRequest}} ->
+            UserId = proplists:get_value(<<"user_id">>, FirstRequest),
+            Module:start_session(UserId, Sock);
+        _ ->
+            {error, cant_parse}
+    end.
